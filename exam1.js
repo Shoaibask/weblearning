14d191a0a04e0774c91454ac0bad8206ea878eb6
Shoaibask <100688921+Shoaibask@users.noreply.github.com>
2022-05-17 15:40:43 +0530

    Add files via upload

---
+++ exam1.js
@@ -0,0 +1,59 @@
+// question number (1)
+let a = 'lata';
+// question number (2)
+let flower = 'rose';
+let tree = 'maple';
+// question number (3)
+x = 'Hardy';
+// question number (4)
+let number = ('2', '4', '6');
+if (number % 2 == 0) {
+    console.log('the number is true');
+} else {
+    console.log('the number is false')
+}
+// question number (5)
+let x = 3
+x++;
+x = x * 2
+x--;
+answerX = 7
+// question number  (6)
+let i = 100
+while (i < 1000) {
+    console.log(i);
+    i++;
+}
+// question number (7)
+let i = 0
+while (i <= 100) {
+    console.log(i)
+    i++
+}
+// question number (9)
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+let getLastElement = numbers[numbers.length - 1];
+console.log(getLastElement)
+// question number (11)
+let b = 10
+let c = 16
+console.log(b * c)
+let x = 2
+let y = 4
+console.log(x % y)
+// question number (12)
+let fruit = "orange";
+if (fruit.length > 5) {
+    console.log("the fruit name has more than five characters.");
+} else {
+    console.log("The fruit name has five characters or less.")
+};
+// question number (13)
+array is special variable which store more than values only one array we can put more values.before array we can used multiple variable
+for values javascript made the easy way and decreased the line.
+// before array used 
+let cars = volvo
+let cars = bmw
+let cars = lamborghini
+// after array used
+let cars = ['volvo', 'bmw', 'lamboghini'...etc]
